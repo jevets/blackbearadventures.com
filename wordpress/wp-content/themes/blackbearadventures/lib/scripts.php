340db1e961108730de6c5c80685e9dce70c5ad41
Jim Jamesson <jim@groovywebdesign.com>
2013-12-15 15:31:47 -0500

    Fix nav fallback, references to Effra font

--- wordpress/wp-content/themes/blackbearadventures/lib/scripts.php
+++ wordpress/wp-content/themes/blackbearadventures/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e74ef5ea93285636ab02c8580ef2f3cf');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '192a1c32a4cb7eb6ebd19fd41fd2a1e7');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
