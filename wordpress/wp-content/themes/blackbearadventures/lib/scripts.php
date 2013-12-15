9d00d4bd4c86c317017a16b0be4a8f6559ba3ce9
Jim Jamesson <jim@groovywebdesign.com>
2013-12-15 15:36:37 -0500

    Final clean up shit

--- wordpress/wp-content/themes/blackbearadventures/lib/scripts.php
+++ wordpress/wp-content/themes/blackbearadventures/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '192a1c32a4cb7eb6ebd19fd41fd2a1e7');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f1e7c9c6ccc6d5e8a83be43e251c9993');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
