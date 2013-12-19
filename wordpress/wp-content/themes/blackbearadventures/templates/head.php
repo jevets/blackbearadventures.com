c1b7c3fb0b218b2b639e83141b537781c696a64f
Steve Jamesson <steve@groovywebdesign.com>
2013-12-19 15:47:00 -0500

    roboto google font, move font import to head.php as a <link> element (faster loading), remove typekit from head.php

--- wordpress/wp-content/themes/blackbearadventures/templates/head.php
+++ wordpress/wp-content/themes/blackbearadventures/templates/head.php
@@ -5,12 +5,15 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?php wp_title('|', true, 'right'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,700|Roboto+Condensed:700">
 
   <?php wp_head(); ?>
 
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
+  <?php /*
   <script type="text/javascript" src="//use.typekit.net/bnx7cgt.js"></script>
   <script type="text/javascript">try{Typekit.load();}catch(e){}</script>  
+  */ ?>
 	<script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
 	<script type="text/javascript">stLight.options({publisher: "513e7a03-b0bf-4db3-96be-483679c5a583", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
 </head>
