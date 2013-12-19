107c6e05b9969e8c19f5d9a71de82a85ae185ff9
Jim Jamesson <jim@groovywebdesign.com>
2013-12-19 15:34:50 -0500

    Change fonts to gfont

--- wordpress/wp-content/themes/blackbearadventures/lib/scripts.php
+++ wordpress/wp-content/themes/blackbearadventures/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5113a084e8fce8431ae645522c383389');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '11cb1c451e89efdfbe14ab02af33ec29');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
