c9bab47182dfc7ab4deb30d3d7df3b71d30f7faf
Steve Jamesson <steve@groovywebdesign.com>
2013-12-26 10:09:57 -0500

    grid-float-break at 1200px (75em), update wpseo plugin

--- wordpress/wp-content/themes/blackbearadventures/lib/scripts.php
+++ wordpress/wp-content/themes/blackbearadventures/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'fdbb82bd1c76d831e2d6d5b88608f2ff');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'fc6e2e4c6c9e380b26b7a7db151e33c7');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
