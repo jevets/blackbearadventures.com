276688241551f5c552c40d70e5715be986e31544
Jim Jamesson <jim@groovywebdesign.com>
2013-12-31 16:13:06 -0500

    Home, nav updates

--- wordpress/wp-content/themes/blackbearadventures/page-home.php
+++ wordpress/wp-content/themes/blackbearadventures/page-home.php
@@ -30,7 +30,8 @@
           <img class="img-responsive img-shadow" src="/media/avid-cyclists.jpg" alt="">
           <h2>Our tours are designed for the Avid Cyclist (You know who you are)</h2>
           <p>Black Bear Adventures bicycle tours are designed for the Avid or serious cycling enthusiast. This means that on a typical day you will cycle 50+ miles, climb in excess of 4000 feet and be in the saddle for 4+ hours. And although our tours are geared towards "Avid" cyclists we don't define avid by the pace you are capable of maintaining, it is demonstrated by a desire and capability to accept the challenge outlined above.</p>
-          <p>We carefully design each tour and select each road to ensure the finest rider experience available...remember at Black Bear Adventures "It IS About the Bike”! Are you ready to accept the Challenge?</p>
+          <p>We carefully design each tour and select each road to ensure the finest rider experience available... remember at Black Bear Adventures "It <em>IS</em> About the Bike”!</p>
+          <p class="lead">Are you ready to accept the Challenge?</p>
         </div><!-- /.col-lg-4 -->
         <div class="col-sm-4">
           <img class="img-responsive img-shadow" src="/media/cycling-support-team.jpg" alt="">
@@ -41,8 +42,8 @@
         <div class="col-sm-4">
           <img class="img-responsive img-shadow" src="/media/m3.jpg" alt="">
           <h2>What We Do and Where We Go</h2>
-          <p>We at Black Bear Adventures consider ourselves experts in the best cycling here in the south; from the historic Natchez Trace to the Blue Ridge Parkway, from the bayou to the bourbon trail and every desirable road in between.</p>
-          <p>But while we are a bicycle vacation with an emphasis on the special cycling here in the south I have been lucky enough to have designed and led trips in 35 states thanks to both the Private Tour and Event Management arms of our company. So don’t hesitate to come to us with an idea or desire to cycle somewhere outside of our core area of operation.</p>
+          <p>We at Black Bear Adventures consider ourselves experts in the best cycling here in the south; from the <a href="/natchez-trace/">historic Natchez Trace</a> to the <a href="/blue-ridge-parkway/">Blue Ridge Parkway</a>, from the bayou to the bourbon trail and every desirable road in between.</p>
+          <p>But while we are a bicycle vacation with an emphasis on the special cycling here in the south I have been lucky enough to have designed and led trips in 35 states thanks to both the <a href="/private-tours/">Private Tour</a> and <a href="/event-management/">Event Management</a> arms of our company. So don’t hesitate to come to us with an idea or desire to cycle somewhere outside of our core area of operation.</p>
           <p><a class="btn btn-default" href="/contact/" role="button">Ready to ride? &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
       </div><!-- /.row -->
@@ -58,12 +59,12 @@
 
       <div class="row featurette">
         <div class="col-sm-7">
-          <h2 class="featurette-heading">Blue Ridge Parkway <span class="text-muted"> Bicycle Tour</span></h2>
-          <p class="lead">Our signature tour...enjoy spectacular views, challenging climbs and thrilling descents along this 469 mile journey from the Shenandoah National Park in Virginia to the Great Smoky Mountains National Park in North Carolina.</p>
+          <h2 class="featurette-heading"><a href="/blue-ridge-parkway/">Blue Ridge Parkway <span class="text-muted"> Bicycle Tour</span></a></h2>
+          <p class="lead">Our signature tour... enjoy spectacular views, challenging climbs and thrilling descents along this 469 mile journey from the Shenandoah National Park in Virginia to the Great Smoky Mountains National Park in North Carolina.</p>
           <p><a class="btn btn-primary" href="/blue-ridge-parkway/" role="button">Trip Details, Itinerary &amp; Pricing &raquo;</a></p>
         </div>
         <div class="col-sm-5">
-          <img class="featurette-image img-responsive img-shadow" src="/media/blue-ridge-parkway.jpg" alt="Generic placeholder image">
+          <a href="/blue-ridge-parkway/"><img class="featurette-image img-responsive img-shadow" src="/media/blue-ridge-parkway.jpg" alt="Generic placeholder image"></a>
         </div>
       </div>
 
@@ -71,10 +72,10 @@
 
       <div class="row featurette">
         <div class="col-sm-5">
-          <img class="featurette-image img-responsive img-shadow" src="/media/h2.jpg" alt="Generic placeholder image">
+          <a href="/natchez-trace/"><img class="featurette-image img-responsive img-shadow" src="/media/h2.jpg" alt="Generic placeholder image"></a>
         </div>
         <div class="col-sm-7">
-          <h2 class="featurette-heading">Natchez Trace Parkway <span class="text-muted"> Bicycle Tour</span></h2>
+          <h2 class="featurette-heading"><a href="/natchez-trace/">Natchez Trace Parkway <span class="text-muted"> Bicycle Tour</span></a></h2>
           <p class="lead">The Natchez Trace is a an unforgettable road that stretches 444-miles from the Mississippi River in Natchez through the Shoals area in Alabama across Tennessee's Cumberland Plateau and on  to Nashville.  This National Scenic Byway is perfectly suited for cycling and one of the finniest two wheel experiences your will ever encounter.</p>
           <p><a class="btn btn-primary" href="/natchez-trace/" role="button">Trip Details, Itinerary &amp; Pricing &raquo;</a></p>
         </div>
@@ -89,7 +90,7 @@
           <p><a class="btn btn-primary" href="/blue-ridge-parkway-epic/" role="button">Trip Details, Itinerary &amp; Pricing &raquo;</a></p>
         </div>
         <div class="col-sm-5">
-          <img class="featurette-image img-responsive img-shadow" src="/media/h3.jpg" alt="Generic placeholder image">
+          <a href="/blue-ridge-parkway-epic/"><img class="featurette-image img-responsive img-shadow" src="/media/h3.jpg" alt="Generic placeholder image"></a>
         </div>
       </div>
 
