181446a7a633ab9780b6a83c0db8cdf2123ebd8a
Steve Jamesson <steve@groovywebdesign.com>
2014-01-05 16:04:53 -0500

    lower grid-float-breakpoint to 800px (50em)

--- wordpress/wp-content/themes/blackbearadventures/lib/scripts.php
+++ wordpress/wp-content/themes/blackbearadventures/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e1db12d4bb31d14652f4e1a1b38262c1');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0505c6361d1bbd8605380a06609bacc7');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
