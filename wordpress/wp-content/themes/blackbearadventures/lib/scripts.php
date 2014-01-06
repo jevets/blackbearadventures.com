61cc7d25783ecf4661c7ee140aa57c3eb7fcf9d9
Steve Jamesson <steve@groovywebdesign.com>
2014-01-06 10:50:11 -0500

    hyphens vendor prefixes

--- wordpress/wp-content/themes/blackbearadventures/lib/scripts.php
+++ wordpress/wp-content/themes/blackbearadventures/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '861376e9b2defa1533d453f5c9e67fb1');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'fdcd590344ee275b5a668d5ef271f2fc');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
