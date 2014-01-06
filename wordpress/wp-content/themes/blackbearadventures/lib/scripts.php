b33bcaad918c5f2a2022b753a2107285837e7724
Steve Jamesson <steve@groovywebdesign.com>
2014-01-06 10:48:38 -0500

    hyphenate hero title

--- wordpress/wp-content/themes/blackbearadventures/lib/scripts.php
+++ wordpress/wp-content/themes/blackbearadventures/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7cb2ec3e804b25d195caaa9c45b72a66');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '861376e9b2defa1533d453f5c9e67fb1');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
