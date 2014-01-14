81acc4135c3655cf8b660b20aa35fc423cd3f7a9
Jim Jamesson <jim@groovywebdesign.com>
2014-01-14 00:59:48 -0500

    tweak northern natchez

--- wordpress/wp-content/themes/blackbearadventures/page-northern-natchez-trace.php
+++ wordpress/wp-content/themes/blackbearadventures/page-northern-natchez-trace.php
@@ -35,37 +35,6 @@
       </div>
     </div>
 
-    <h2>Included in the Trip Price</h2>
-    <h3>Lodging</h3>
-    <p>5 nights lodging beginning on Saturday May the 17th and concluding on Wednesday May the 21st</p>
-    <h3>Food & Snacks</h3>
-    <ul>
-      <li>Breakfast and Lunch beginning with breakfast on Sunday the 18th and concluding with breakfast on Thursday the 22nd</li>
-      <li>All snacks and beverages during each day’s ride</li>
-    </ul>
-    <h3>Support</h3>
-    <ul>
-      <li>The knowledge, support and camaraderie of experienced guides</li>
-      <li>Mechanical support as needed during your trip</li>
-      <li>Daily route support plus detailed route maps w/ profile data</li>
-      <li>Ride support vehicle accompanies Tour</li>
-      <li>Shuttle service from Nashville to Florence on May the 17th and to the Nashville airport after conclusion of Tour on Thursday May the 22nd</li>
-      <li>All necessary transportation and luggage transfers during your trip</li>
-    </ul>
-    <h3>Extras</h3>
-    <ul>
-      <li>BBA Swag including water bottles, cycling jersey and other goodies...</li>
-    </ul>
-
-    <h2>Not Included</h2>
-    <ul>
-      <li>Airfare to and from the trip pick-up/drop-off locations</li>
-      <li>Alcohol</li>
-      <li>BBA rental bike - in 2014 we will be offering the beautifully crafted <a href="http://aluboobikes.com/shop/r-series/">Aluboo Bikes</a> for $45/day</li>
-      <li>Personal items purchased during the trip</li>
-      <li>Gratuities for your Black Bear Adventures Guides</li>
-    </ul>
-
 
     <div class="col-sm-4 sidebar">
       <div class="well">
@@ -102,6 +71,7 @@
         <ul class="nav nav-tabs">
             <li class="active"><a href="#photos" data-toggle="tab">Tour Gallery</a></li>
             <li><a href="#intinerary" data-toggle="tab">Itinerary</a></li>
+            <li><a href="#included" data-toggle="tab">What's Included</a></li>
             <li><a href="#interest" data-toggle="tab">Points of Interest</a></li>
         </ul>
     </div>
@@ -254,6 +224,41 @@
           </div>
 
         </div>
+
+
+        <div class="tab-pane" id="included">
+          <h2>Included in the Trip Price</h2>
+          <h3>Lodging</h3>
+          <p>5 nights lodging beginning on Saturday May the 17th and concluding on Wednesday May the 21st</p>
+          <h3>Food & Snacks</h3>
+          <ul>
+            <li>Breakfast and Lunch beginning with breakfast on Sunday the 18th and concluding with breakfast on Thursday the 22nd</li>
+            <li>All snacks and beverages during each day’s ride</li>
+          </ul>
+          <h3>Support</h3>
+          <ul>
+            <li>The knowledge, support and camaraderie of experienced guides</li>
+            <li>Mechanical support as needed during your trip</li>
+            <li>Daily route support plus detailed route maps w/ profile data</li>
+            <li>Ride support vehicle accompanies Tour</li>
+            <li>Shuttle service from Nashville to Florence on May the 17th and to the Nashville airport after conclusion of Tour on Thursday May the 22nd</li>
+            <li>All necessary transportation and luggage transfers during your trip</li>
+          </ul>
+          <h3>Extras</h3>
+          <ul>
+            <li>BBA Swag including water bottles, cycling jersey and other goodies...</li>
+          </ul>
+
+          <h2>Not Included</h2>
+          <ul>
+            <li>Airfare to and from the trip pick-up/drop-off locations</li>
+            <li>Alcohol</li>
+            <li>BBA rental bike - in 2014 we will be offering the beautifully crafted <a href="http://aluboobikes.com/shop/r-series/">Aluboo Bikes</a> for $45/day</li>
+            <li>Personal items purchased during the trip</li>
+            <li>Gratuities for your Black Bear Adventures Guides</li>
+          </ul>
+        </div>
+
 
         <div class="tab-pane" id="interest">
           <h2>Points of Interest</h2>
