f59f78bd96e2df5b2d94836f19262633c2e76dfa
Steve Jamesson <steve@groovywebdesign.com>
2014-01-14 07:59:37 -0500

    center img-responsives

--- wordpress/wp-content/themes/blackbearadventures/lib/scripts.php
+++ wordpress/wp-content/themes/blackbearadventures/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '097de82620051438e9fc3fdadc1bb262');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'fd7bb29120257bf53d071ec02232abe6');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
