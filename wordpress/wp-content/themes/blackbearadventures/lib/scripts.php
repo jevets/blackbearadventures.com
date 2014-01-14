011bf71961af5242a0b3196199fd9c2173fbbd07
Steve Jamesson <steve@groovywebdesign.com>
2014-01-14 07:58:21 -0500

    centered img-responsives

--- wordpress/wp-content/themes/blackbearadventures/lib/scripts.php
+++ wordpress/wp-content/themes/blackbearadventures/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'fd03508a03c1b5169afb974d9fce0099');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '097de82620051438e9fc3fdadc1bb262');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
