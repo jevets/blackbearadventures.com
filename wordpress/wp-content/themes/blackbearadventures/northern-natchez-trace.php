008af3a0dbc1e689568bf93ca7119f51e986a961
Jim Jamesson <jim@groovywebdesign.com>
2014-01-14 00:54:32 -0500

    Add and updated northern natchez trace tour

---
+++ wordpress/wp-content/themes/blackbearadventures/northern-natchez-trace.php
@@ -0,0 +1,274 @@
+
+<div class="container">
+  <div class="row">
+
+    <?php get_template_part('templates/page-header') ?>
+
+    <div class="col-sm-8">
+      <h2>Overview</h2>
+      <p class="lead">This years Northern Natchez Trace bicycle tour has been modified to increase the number of nights we stay at the wonderful inns chosen for this tour and to cycle some of the fabulous back roads surrounding the Natchez Trace. The combination of inn's, extra curricular activities and cycling make this one the very best tours we have to offer.</p>
+
+      <hr />
+
+      <div class="row">
+          <div class="col-sm-4">
+              <h4>Tour Mileage</h4>
+              <p class="lead"><span class="label label-primary label-pad"><span class="glyphicon glyphicon-road"></span> 500+ Miles</span></p>
+          </div>
+          <div class="col-sm-4">
+              <h4>Trip Length</h4>
+              <p class="lead"><span class="label label-primary label-pad"><span class="glyphicon glyphicon-time"></span> 6 Days/5 Nights</span></p>
+          </div>
+          <div class="col-sm-4">
+              <h4>Trip Begins</h4>
+              <p class="lead"><span class="label label-primary label-pad"><span class="glyphicon glyphicon-map-marker"></span> Nashville, TN</span></p>
+          </div>
+      </div>
+
+      <hr>
+
+      <div id="sharethis" class="hidden-print">    
+        <span class='st_twitter_hcount' displayText='Tweet'></span>
+        <span class='st_digg_hcount' displayText='Digg'></span>
+        <span class='st_pinterest_hcount' displayText='Pinterest'></span>
+        <span class='st_facebook_hcount' displayText='Facebook'></span>
+      </div>
+    </div>
+
+    <h2>Included in the Trip Price</h2>
+    <h3>Lodging</h3>
+    <p>5 nights lodging beginning on Saturday May the 17th and concluding on Wednesday May the 21st</p>
+    <h3>Food & Snacks</h3>
+    <ul>
+      <li>Breakfast and Lunch beginning with breakfast on Sunday the 18th and concluding with breakfast on Thursday the 22nd</li>
+      <li>All snacks and beverages during each day’s ride</li>
+    </ul>
+    <h3>Support</h3>
+    <ul>
+      <li>The knowledge, support and camaraderie of experienced guides</li>
+      <li>Mechanical support as needed during your trip</li>
+      <li>Daily route support plus detailed route maps w/ profile data</li>
+      <li>Ride support vehicle accompanies Tour</li>
+      <li>Shuttle service from Nashville to Florence on May the 17th and to the Nashville airport after conclusion of Tour on Thursday May the 22nd</li>
+      <li>All necessary transportation and luggage transfers during your trip</li>
+    </ul>
+    <h3>Extras</h3>
+    <ul>
+      <li>BBA Swag including water bottles, cycling jersey and other goodies...</li>
+    </ul>
+
+    <h2>Not Included</h2>
+    <ul>
+      <li>Airfare to and from the trip pick-up/drop-off locations</li>
+      <li>Alcohol</li>
+      <li>BBA rental bike - in 2014 we will be offering the beautifully crafted <a href="http://aluboobikes.com/shop/r-series/">Aluboo Bikes</a> for $45/day</li>
+      <li>Personal items purchased during the trip</li>
+      <li>Gratuities for your Black Bear Adventures Guides</li>
+    </ul>
+
+
+    <div class="col-sm-4 sidebar">
+      <div class="well">
+        <h3>Cost</h3>
+        <p><span class="price">$1595</span><br>
+        Double Occupancy</p>
+        <p><span class="price">$2140</span><br>
+        Single Occupancy</p>
+
+        <hr />
+
+        <h4>Deposit Required</h4>
+        <p><span class="price">$250</span><br>
+        Per person with the remainder due 60 days before the tour start date.</p>
+
+        <hr />
+
+        <h3><span class="glyphicon glyphicon-calendar"></span> 2014 Dates</h3>
+        <h4>6 Days from Saturday May 17 - Thursday May 22</h4>
+        <hr>
+
+        <p class="hidden-print"><button class="btn btn-large btn-default btn-block" onClick="window.print()"><span class="glyphicon glyphicon-print"></span> Print Complete Tour Itinerary</button></p>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
+
+<!-- TABS -->
+<div class="container">
+    <div class="row">
+        <ul class="nav nav-tabs">
+            <li class="active"><a href="#photos" data-toggle="tab">Tour Gallery</a></li>
+            <li><a href="#intinerary" data-toggle="tab">Itinerary</a></li>
+            <li><a href="#interest" data-toggle="tab">Points of Interest</a></li>
+        </ul>
+    </div>
+    <!-- Tab panes -->
+    <div class="tab-content">
+        
+        <div class="tab-pane active" id="photos">
+          <h2>Northern Natchez Trace Tour Gallery</h2>
+          <div class="hidden-print">
+            <?php the_content() ?>
+          </div>
+          <p class="visible-print">For trip photos of the Northern Natchez Trace Tour, please visit <a href="http://blackbearadventures.com/northern-natchez-trace/">http://blackbearadventures.com/northern-natchez-trace/</a></p>
+        </div>
+
+        <div class="tab-pane" id="intinerary">
+          <h2>Itinerary</h2>
+
+          <div class="row">
+            <div class="col-sm-6">
+              <h3><span class="label label-primary">Day 1</span> Saturday May 17: Florence, AL</h3>
+              <p>We shuttle from Nashville to Florence in the afternoon. After settling in to our rooms we will gather to review the upcoming itinerary before heading into town for dinner.</p>
+            </div>
+            <div class="col-sm-2">
+              <h4><span class="glyphicon glyphicon-cutlery"></span> Meals</h4>
+              <p>NA</p>
+            </div>
+            <div class="col-sm-2">
+              <h4><span class="glyphicon glyphicon-road"></span> Mileage</h4>
+              <span class="label label-default">0 Miles</span>
+            </div>
+            <div class="col-sm-2">
+              <h4><span class="glyphicon glyphicon-home"></span> Stay</h4>
+              <p><a href="http://www.marriott.com/hotels/travel/mslmc-marriott-shoals-hotel-and-spa/" target="_blank"><img src="/media/asdfasdf.jpg" class="img-responsive hidden-print" alt="Marriott Shoals Hotel &amp; Spa!" width="100%"><br>
+              Marriott Shoals Hotel &amp; Spa! <span class="glyphicon glyphicon-new-window"></span></a></p>
+            </div>
+          </div>
+
+          <hr />
+
+          <div class="row">
+            <div class="col-sm-6">
+              <h3><span class="label label-primary">Day 2</span> Sunday May 18: Florence, AL loop</h3>
+              <p>There are numerous options today to experience the beautiful backroads of north western Alabama.</p>
+            </div>
+            <div class="col-sm-2">
+              <h4><span class="glyphicon glyphicon-cutlery"></span> Meals</h4>
+              <p><span class="label label-default">Breakfast</span></p>
+              <p><span class="label label-default">Lunch</span></p>
+            </div>
+            <div class="col-sm-2">
+              <h4><span class="glyphicon glyphicon-road"></span> Mileage</h4>
+              <span class="label label-default">Up to 65 miles</span>
+            </div>
+            <div class="col-sm-2">
+              <h4><span class="glyphicon glyphicon-home"></span> Stay</h4>
+              <p><a href="http://www.marriott.com/hotels/travel/mslmc-marriott-shoals-hotel-and-spa/" target="_blank"><img src="/media/asdfasdf.jpg" class="img-responsive hidden-print" alt="Marriott Shoals Hotel &amp; Spa!" width="100%"><br>
+              Marriott Shoals Hotel &amp; Spa! <span class="glyphicon glyphicon-new-window"></span></a></p>
+            </div>
+          </div>
+
+          <hr />
+
+          <div class="row">
+            <div class="col-sm-6">
+              <h3><span class="label label-primary">Day 3</span> Monday May 19: Florence, AL to Columbia, TN</h3>
+              <p>Our first day on the Natchez Trace! We shuttle out from the hotel before entering the Trace where we will quickly cross the state line into Tennessee. Today’s rolling terrain will provide a wonderful initial experience along this incredible <a href="http://www.tdot.state.tn.us/Byways/scenic-byway-natchez-trace-parkway.aspx">national scenic byway</a>.</p>
+            </div>
+            <div class="col-sm-2">
+              <h4><span class="glyphicon glyphicon-cutlery"></span> Meals</h4>
+              <p><span class="label label-default">Breakfast</span></p>
+              <p><span class="label label-default">Lunch</span></p>
+            </div>
+            <div class="col-sm-2">
+              <h4><span class="glyphicon glyphicon-road"></span> Mileage</h4>
+              <span class="label label-default">Up to 75 miles</span>
+            </div>
+            <div class="col-sm-2">
+              <h4><span class="glyphicon glyphicon-home"></span> Stay</h4>
+              <p><a href="http://www.ihg.com/holidayinnexpress/hotels/us/en/columbia/clbcp/hoteldetail?qCiD=08&qCiMy=042014&qCoD=09&qCoMy=042014&qDest=Columbia%2C+Tn%2C+United+States&qHtlC=bnaus&qAdlt=1&qChld=0&qRms=1&qIta=99626424&qPSt=0&qRtP=6CBARC&qSmP=3&qRmP=3.O.T.X.C&qWch=0&qSHp=1&qBrs=hi.ex.rs.cv.ic.cp.in.vn.sb.cw.6c&qSrt=BRAND_SORT&qRad=30&qRdU=1&qRpp=10&qRRSrt=rt&qLat=35.615&qLng=-87.0353&srb_u=1" target="_blank"><img src="/media/asdfasdf.jpg" class="img-responsive hidden-print" alt="Holiday Inn Express" width="100%"><br>
+              Holiday Inn Express <span class="glyphicon glyphicon-new-window"></span></a></p>
+            </div>
+          </div>
+
+          <hr />
+
+          <div class="row">
+            <div class="col-sm-6">
+              <h3><span class="label label-primary">Day 4</span> Tuesday May 20: Columbia, TN to Leipers Fork, TN</h3>
+              <p>Another glorious day along the Natchez Trace! We once again shuttle back out to the Trace to begin our cycling, todays route continues to wind along the Cumberland Plateau, the southern most stretches of the Appalachians and ends in the historic community of Leipers Fork.</p>
+            </div>
+            <div class="col-sm-2">
+              <h4><span class="glyphicon glyphicon-cutlery"></span> Meals</h4>
+              <p><span class="label label-default">Breakfast</span></p>
+              <p><span class="label label-default">Lunch</span></p>
+            </div>
+            <div class="col-sm-2">
+              <h4><span class="glyphicon glyphicon-road"></span> Mileage</h4>
+              <span class="label label-default">Up to 65 miles</span>
+            </div>
+            <div class="col-sm-2">
+              <h4><span class="glyphicon glyphicon-home"></span> Stay</h4>
+              <p><a href="http://www.ihg.com/holidayinnexpress/hotels/us/en/brentwood/bnana/hoteldetail?qCiD=08&qCiMy=042014&qCoD=09&qCoMy=042014&qDest=Franklin,+Tn,+United+States&qHtlC=bnaus&qAdlt=1&qChld=0&qRms=1&qIta=99626424&qPSt=0&qRtP=6CBARC&qSmP=3&qRmP=3.O.T.X.C&qWch=0&qSHp=1&qBrs=hi.ex.rs.cv.ic.cp.in.vn.sb.cw.6c&qSrt=BRAND_SORT&qRad=30&qRdU=1&qRpp=10&qRRSrt=rt&qLat=35.925&qLng=-86.8689&srb_u=1" target="_blank"><img src="/media/hilton-garden-inn.jpg" class="img-responsive hidden-print" alt="Hilton Garden Inn" width="100%"><br>
+              Holiday Inn Express <span class="glyphicon glyphicon-new-window"></span></a></p>
+            </div>
+          </div>
+
+          <hr />
+
+          <div class="row">
+            <div class="col-sm-6">
+              <h3><span class="label label-primary">Day 5</span> Wednesday May 21: Leipers Fork, TN to Nashville</h3>
+              <p>We shuttle back to the Trace and set out along the backroads including stints along the Old “Natchez Trace” before turing north towards Nashville where we celebrate a wonderful and successful trip with Black Bear Adventures.</p>
+            </div>
+            <div class="col-sm-2">
+              <h4><span class="glyphicon glyphicon-cutlery"></span> Meals</h4>
+              <p><span class="label label-default">Breakfast</span></p>
+              <p><span class="label label-default">Lunch</span></p>
+            </div>
+            <div class="col-sm-2">
+              <h4><span class="glyphicon glyphicon-road"></span> Mileage</h4>
+              <span class="label label-default">Up to 65 miles</span>
+            </div>
+            <div class="col-sm-2">
+              <h4><span class="glyphicon glyphicon-home"></span> Stay</h4>
+              <p><a href="#" target="_blank"><img src="/media/asdfasdf.jpg" class="img-responsive hidden-print" alt="Marriott Courtyard" width="100%"><br>
+              Marriott Courtyard <span class="glyphicon glyphicon-new-window"></span></a></p>
+            </div>
+          </div>
+
+          <hr />
+
+          <div class="row">
+            <div class="col-sm-6">
+              <h3><span class="label label-primary">Day 6</span> Thursday May 22: Nashville, TN</h3>
+              <p>After breakfast where we reflect on the joys, laughter and challenge of these previous few days. We will offer shuttles to Nashville International Airport.</p>
+            </div>
+            <div class="col-sm-2">
+              <h4><span class="glyphicon glyphicon-cutlery"></span> Meals</h4>
+              <p><span class="label label-default">Breakfast</span></p>
+            </div>
+            <div class="col-sm-2">
+              <h4><span class="glyphicon glyphicon-road"></span> Mileage</h4>
+              <span class="label label-default">0 miles</span>
+            </div>
+            <div class="col-sm-2">
+              <h4><span class="glyphicon glyphicon-home"></span> Stay</h4>
+              <p><a href="http://www.marriott.com/hotels/travel/mslmc-marriott-shoals-hotel-and-spa/" target="_blank"><img src="/media/marriott-shoals-hotel-spa.jpg" class="img-responsive hidden-print" alt="Marriott Shoals Hotel &amp; Spa" width="100%"><br>
+              Marriott Shoals Hotel &amp; Spa <span class="glyphicon glyphicon-new-window"></span></a></p>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="tab-pane" id="interest">
+          <h2>Points of Interest</h2>
+          <p>Below are a few links that will help you research the area associated with the Natchez Trace Parkway bicycle tour.</p>
+          <ul>
+            <li><a href="http://www.natchez.ms.us/">Natchez, MS</a></li>
+            <li><a href="http://www.visitjackson.com/">Jackson, MS</a></li>
+            <li><a href="http://www.tupelo.net/">Tupelo, MS</a></li>
+            <li><a href="http://www.flo-tour.org/">Florence, AL</a></li>
+            <li><a href="http://www.nashvillecvb.com/">Nashville, TN</a></li>
+            <li><a href="http://www.nps.gov/natr/index.htm">Natchez Trace Parkway</a></li>
+            <li><a href="http://www.scenictrace.com/">Scenic Trace</a></li>
+          </ul>
+        </div>
+
+    </div>
+</div>
+<!-- TABS -->
